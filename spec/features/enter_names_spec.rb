93b943a47329c822e31008a4fa4641b464d05f94
Ben Scott <bengscott2@gmail.com>
2019-06-12 15:37:19 +0100

    Added names and HP for player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,14 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Dave'
     fill_in :player_2_name, with: 'Mittens'
     click_button 'Submit'
-    expect(page).to have_content 'Dave vs. Mittens'
+    expect(page).to have_content "Dave", "Mittens"
+  end
+
+  scenario 'See Player 2 Hit Points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Mittens HP: 100/100'
   end
 end
